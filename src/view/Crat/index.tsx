855e460ceb21bb9b7bba9efe5f01d70d89242892
HibaRehanIT36 <hibarehan36@gmail.com>
2023-11-01 22:23:10 +0300

    add cart component

---
+++ src/view/Crat/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import CartItem from "./CartItem";
+import CartFooter from "./CartFooter";
+import { UseGetCartHandler } from "../../hooks/UseGetCartHandler";
+import { cartItemType, cartPropsType } from "../../types";
+
+function Cart(props: cartPropsType) {
+  const { CartItems, deleteCartItem, editCartItem, buyCartItems } = props;
+  const [showCart, setShowCart] = useState(false);
+
+  return (
+    <>
+      {CartItems.length > 0 && (
+        <div className={`container-cart ${!showCart ? "hidden-cart" : ""}`}>
+          <div className="header-cart">
+            <h2>your cart</h2>
+          </div>
+          <div className="cart-items">
+            {CartItems.map((cartItem) => (
+              <CartItem
+                cartItemInfo={cartItem}
+                editCartItem={editCartItem}
+                deleteCartItem={deleteCartItem}
+              />
+            ))}
+          </div>
+          <CartFooter CartItems={CartItems} buyCartItems={buyCartItems} />
+        </div>
+      )}
+      <button
+        className={`toggel-cart-button ${CartItems.length === 0 ? "" : "has-items-number"}`}
+        items-number={CartItems.length}
+        onClick={() =>
+          CartItems.length > 0
+            ? setShowCart(!showCart)
+            : window.alert("your cart is empty")
+        }
+      />
+    </>
+  );
+}
+
+export default Cart;
