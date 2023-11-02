f0daf9d6c0eb55299308bf9f78eeb97e9fd885cc
HibaRehanIT36 <hibarehan36@gmail.com>
2023-11-02 12:36:10 +0300

    refactor useGetProductHandler hook

--- src/hooks/UseGetProductHandler.ts
+++ src/hooks/UseGetProductHandler.ts
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { productItemType } from "../types";
+import { productItemType, productStatus } from "../types";
 
 export function UseGetProductHandler() {
   const [products, setProducts] = useState<productItemType[]>([]);
-
-    useEffect(() => {
-      fetch("https://api.escuelajs.co/api/v1/products")
+  const [currentPage, setCurrentPage] = useState(1);
+  
+  useEffect(() => {
+    fetch(`https://api.escuelajs.co/api/v1/products?offset=${currentPage-1}&limit=4`)
       .then((response) => response.json())
       .then((productData) => {
-        const slicedProducts = productData.slice(0, 10).map((item: any) => ({
-          id: item.id,
-          title: item.title,
-          price: item.price,
-          description: item.description,
-          imagePath: item.images[0],
-        }));
-  
-        setProducts(slicedProducts);
+          const slicedProducts = productData.slice(0, 10).map((item: any) => ({
+            id: item.id,
+            title: item.title.slice(0, 10),
+            price: item.price,
+            description: item.description.slice(0, 30),
+            imagePath: item.images[0],
+            status: "normal",
+          }));
+          setProducts(slicedProducts);  
+
+                
       })
       .catch((error) => {
-        console.error("Error fetching items:", error);
+        window.alert("Error fetching products");
       });
-    }, []);
-
-  return { products };
+  }, [currentPage]);
+  const changeProductStatus = (updatedStatus: productStatus, id: number) => {
+    const updatedProducts = [...products];
+    const index = updatedProducts.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      updatedProducts[index].status = updatedStatus;
+      setProducts(updatedProducts);
+    }
+    return { products, changeProductStatus };
+  };
+  return { products, changeProductStatus , currentPage, setCurrentPage };
 }
