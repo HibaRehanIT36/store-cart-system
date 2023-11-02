f6d7712f8600a05259a78bad1c39e93e547bc2bc
HibaRehanIT36 <hibarehan36@gmail.com>
2023-11-02 12:40:59 +0300

    refactor product components

--- src/view/Products/Item.tsx
+++ src/view/Products/Item.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { productItemProps } from "../../types";
-
-function Item(props:productItemProps) {
+import noImage from "../../assets/noImage.png";
+function Item(props: productItemProps) {
   const { itemInfo, addCartItem } = props;
-  const { id, imagePath, title, description, price } = itemInfo;
+  const { id, imagePath, title, description, price, status } = itemInfo;
+ 
 
   return (
-    <div className="item">
+    <div className="item" tag-data={status}>
       <span className="name-item">{title}</span>
       <span className="description-item">{description}</span>
-      <img src={imagePath} alt="" className="img-item" />
+      <img
+        src={imagePath.includes("any") ? noImage : imagePath}
+        alt=""
+        className="img-item"
+      />
       <span className="price-item">${price}</span>
       <button
         className="button-item"
-        onClick={() => addCartItem({ id, title, price , imagePath })}
+        onClick={() => addCartItem({ id, title, price, imagePath, status })}
       >
         add to cart
       </button>
--- src/view/Products/index.tsx
+++ src/view/Products/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Item from "./Item";
-import { UseGetProductHandler } from "../../hooks/UseGetProductHandler";
 import { productPropsType } from "../../types";
+import Pagination from "../../layout/Pagination";
 
 function Products(props: productPropsType) {
-  const { products } = UseGetProductHandler();
-  const { addCartItem } = props;
+  const { addCartItem, products, currentPage, setCurrentPage } = props;
   return (
     <div className="container-items">
       {products.length > 0 ? (
-        products.map((item) => (
-          <Item itemInfo={item} addCartItem={addCartItem} />
-        ))
+        <>
+          {products.map((item) => (
+            <Item itemInfo={item} addCartItem={addCartItem} />
+          ))}
+          <Pagination
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            totalNumber={products.length}
+          />
+        </>
       ) : (
         <p className="loading-text">product is loading ...</p>
       )}
