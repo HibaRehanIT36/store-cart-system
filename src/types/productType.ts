92823780d66a2c71044db7382d96b31355a8a1d3
HibaRehanIT36 <hibarehan36@gmail.com>
2023-11-02 12:39:19 +0300

    Update productType.ts

--- src/types/productType.ts
+++ src/types/productType.ts
@@ -4,17 +4,22 @@ export type productItemType = {
   title: string;
   description: string;
   price: number;
+  status:productStatus;
 };
-
+export type productStatus ="added"|"bought"|"normal" ;
 export type addedProductType = {
   id: number;
   title: string;
   price: number;
   imagePath: string;
+  status:productStatus;
 };
 
 export type productPropsType = {
   addCartItem: (params: addedProductType) => void;
+  products:productItemType[];
+  currentPage:number ;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export type productItemProps={
