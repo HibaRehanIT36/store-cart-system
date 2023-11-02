253922ed74a6d3362acc4dfdf4c1a13b91f9d92f
HibaRehanIT36 <hibarehan36@gmail.com>
2023-11-02 12:41:18 +0300

    update home component

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -3,15 +3,23 @@ import Products from "../Products";
 import Cart from "../Crat";
 import Header from "../../layout/Header";
 import { UseGetCartHandler } from "../../hooks/UseGetCartHandler";
+import { UseGetProductHandler } from "../../hooks/UseGetProductHandler";
 
 function Home() {
+  const { products, changeProductStatus, currentPage, setCurrentPage } =
+    UseGetProductHandler();
   const { CartItems, addCartItem, editCartItem, deleteCartItem, buyCartItems } =
-    UseGetCartHandler();
+    UseGetCartHandler(changeProductStatus);
   return (
     <div>
       <Header />
       <div className="container">
-        <Products addCartItem={addCartItem} />
+        <Products
+          addCartItem={addCartItem}
+          products={products}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
         <Cart
           CartItems={CartItems}
           editCartItem={editCartItem}
